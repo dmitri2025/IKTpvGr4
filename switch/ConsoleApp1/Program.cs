84710fd6004d9b02170d67465393a15cef657083
Opilane TTHK <>
2025-10-16 10:26:23 +0300

    init

---
+++ switch/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string input = Console.ReadLine();
+
+            switch (input) 
+            {
+                case "a":
+                    Console.WriteLine("Sisestasid tähe a");
+                    break;
+
+                case "e":
+                    Console.WriteLine("Sisestasid tähe e");
+                    break;
+
+                case "i":
+                    Console.WriteLine("Sisestasid tähe i");
+                    break;
+
+                default:
+                    Console.WriteLine("Ei ole vokaal");
+                    break;
+            }
+        }
+    }
+} 
