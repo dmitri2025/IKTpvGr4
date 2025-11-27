37c214c90417cf54cdf65975bfdec9cfb70fce11
Opilane TTHK <opilane@tthk.ee>
2025-11-27 11:06:08 +0200

    DiameeterMm

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            {
+                double muendiDiameeterMm = 25.75;
+
+                double maaRaadiusMm = 6371e3 * 1000;
+
+                double maaUmbermootMm = 2 * Math.PI * maaRaadiusMm;
+
+                double muunteKokku = maaUmbermootMm / muendiDiameeterMm;
+
+                Console.WriteLine($"Maa ümbermõõt: {maaUmbermootMm:n0} mm");
+                Console.WriteLine($"Mündid mahuksid ümber Maa: {muunteKokku:n0}");
+            }
+        }
+
+    } 
+}
