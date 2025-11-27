16f8a177355ba8215184a6708f6629e1d1a5e4a8
Opilane TTHK <opilane@tthk.ee>
2025-11-27 09:53:37 +0200

    Int loop =

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp55
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int loop = 2;
+
+            for (int i = 0; i < loop; i++) 
+            {
+            Console.WriteLine("Loop " + i);
+            Console.Beep();
+            }
+
+        }
+    }
+}
