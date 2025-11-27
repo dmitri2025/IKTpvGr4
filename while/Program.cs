94157c5a14f3fcf7063e909b83dc09dd0a5a1319
Opilane TTHK <opilane@tthk.ee>
2025-11-27 09:43:27 +0200

    while
    wqe

---
+++ while/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int a = 1;
+
+            while (a <= 5) 
+            {
+                int b = 1;
+                while (b <= 1)
+                {
+                Console.Write(b + " ");
+                b++;
+                }
+
+                Console.WriteLine();
+                a++;
+            } 
+        }
+    }
+}
