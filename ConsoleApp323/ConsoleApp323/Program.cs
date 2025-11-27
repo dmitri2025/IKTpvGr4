717a1155e2c90c58e3885275c36f60c8f31510f6
Opilane TTHK <opilane@tthk.ee>
2025-11-27 11:28:42 +0200

    calculation

---
+++ ConsoleApp323/ConsoleApp323/Program.cs
@@ -0,0 +1,23 @@
+﻿calculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Sulgude kasutamine");
+            Console.WriteLine("Kui palju on: 3+8 / (4 - 2) * 4 = ?");
+
+            double calculation1 = 3 + 8 / (4 - 2) * 4;
+            double calculation2 = 3 + 8 / 4 - 2 * 4;
+            double calculation3 = (3 + 8) / (4 - 2) * 4;
+            double calculation4 = 3 + 8 / ((4 - 2) * 4);
+
+            Console.WriteLine("3 + 8 / (4 - 2) * 4 = " + calculation1);
+            Console.WriteLine("3 + 8 / 4 - 2 * 4 = " + calculation2);
+            Console.WriteLine("(3 + 8) / (4 - 2) * 4 = " + calculation3);
+            Console.WriteLine("3 + 8 / ((4 - 2) * 4) = " + calculation4);
+        }
+    }
+}
